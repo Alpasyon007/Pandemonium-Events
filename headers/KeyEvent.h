8d31d6c91857e80ac57af0a697d159110b3f3933
Alpasyon007 <alpasyon07@gmail.com>
2022-11-03 02:16:56 +0000

    Create Events System for Pandemonium Engine

---
+++ headers/KeyEvent.h
@@ -0,0 +1,56 @@
+#ifndef KEY_EVENT_H
+#define KEY_EVENT_H
+
+#include "ppch.h"
+#include "Event.h"
+
+namespace Pandemonium {
+	class PANDEMONIUM_API KeyEvent : public Event {
+		public:
+			inline int GetKeyCode() const { return m_KeyCode; }
+
+			virtual int GetCategoryFlags() const override ( return EventCategoryKeyboard | EventCategoryInput; )
+		protected:
+			KeyEvent(int keycode) : m_KeyCode(keycode) {}
+
+			int m_KeyCode;
+	};
+
+	class PANDEMONIUM_API KeyPressedEvent : public KeyEvent {
+		public:
+			KeyPressedEvent(int keycode, int repeatCound) : KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+			inline int GetRepeatCount() const { return m_RepeatCount; }
+
+			std::string ToString() const override {
+				std::stringstream ss;
+				ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeat)";
+				return ss.str();
+			}
+
+			static EventType GetStaticType() { return EventType::KeyPressed; }
+			virtual EventType GetEventType() const override { return GetStaticType(); }
+			virtual const char* GetName() const override { return "KeyPressed"; }
+
+		private:
+			int m_RepeatCount;
+	};
+
+	class PANDEMONIUM_API KeyReleasedEvent : public KeyEvent {
+		public:
+			KeyReleasedEvent(int keycode) : KeyEvent(keycode) {}
+
+			std::string ToString() const override {
+				std::stringstream ss;
+				ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeat)";
+				return ss.str();
+			}
+
+			static EventType GetStaticType() { return EventType::KeyReleased; }
+			virtual EventType GetEventType() const override { return GetStaticType(); }
+			virtual const char* GetName() const override { return "KeyReleased"; }
+	}
+}
+
+
+#endif /* KEY_EVENT_H */
---
+++ headers/MouseEvent.h
@@ -0,0 +1,96 @@
+#ifndef MOUSE_EVENT_H
+#define MOUSE_EVENT_H
+
+#include "ppch.h"
+#include "Event.h"
+
+namespace Pandemonium {
+	class PANDEMONIUM_API MouseMoveEvent : public Event {
+	public:
+		MouseMoveEvent(float x, float y) : m_MouseX(x), m_MouseY(y) {}
+
+		inline float GetX() const { return m_MouseX; }
+		inline float GetY() const { return m_MouseY; }
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "MouseMovedEvent: " << m_MouseX << ", " << m_MouseY;
+			return ss.str();
+		}  
+
+		static EventType GetStaticType() { return EventType::MouseMoved; }
+		virtual EventType GetEventType() const override { return GetStaticType(); }
+		virtual const char* GetName() const override { return "MouseMoved"; }
+		virtual int GetCategoryFlags() const override ( return EventCategoryMouse | EventCategoryInput; )
+	private:
+		float m_MouseX, m_MouseY;
+	}
+
+	class PANDEMONIUM_API MouseScrolledEvent : public Event {
+	public:
+		MouseScrolledEvent(float xOffset, float yOffset) : m_XOffset(xOffset), m_YOffset(yOffset) {}
+ 
+		inline float GetXOffset() const { return m_XOffset; }
+		inline float GetYOffset() const { return m_YOffset; }
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "MouseScrolledEvent: " << m_XOffset << ", " << m_YOffset;
+			return ss.str();
+		} 
+
+		static EventType GetStaticType() { return EventType::MouseScrolled; }
+		virtual EventType GetEventType() const override { return GetStaticType(); }
+		virtual const char* GetName() const override { return "MouseScrolled"; }
+		virtual int GetCategoryFlags() const override ( return EventCategoryMouse | EventCategoryInput; )
+	private:
+		float m_XOffsety, m_YOffset;
+	}
+
+	class PANDEMONIUM_API MouseButtonEvent : public Event {
+	public:
+		inline int GetMouseButton() const { retrun m_Button; }
+		
+		virtual int GetCategoryFlags() const override ( return EventCategoryMouse | EventCategoryInput; )
+	protected:
+		MouseButtonEvent(int button) : m_Button(button) {}
+
+		int m_Button;
+	}
+
+	class PANDEMONIUM_API MouseButtonPressedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonPressedEvent(const MouseCode button) : MouseButtonEvent(button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseButtonPressedEvent: " << m_Button;
+			return ss.str();
+		}
+
+		static EventType GetStaticType() { return EventType::MouseButtonPressed; }
+		virtual EventType GetEventType() const override { return GetStaticType(); }
+		virtual const char* GetName() const override { return "MouseButtonPressed"; }
+	};
+
+	class PANDEMONIUM_API MouseButtonReleasedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonReleasedEvent(const MouseCode button) : MouseButtonEvent(button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseButtonReleasedEvent: " << m_Button;
+			return ss.str();
+		}
+
+		static EventType GetStaticType() { return EventType::MouseButtonReleased; }
+		virtual EventType GetEventType() const override { return GetStaticType(); }
+		virtual const char* GetName() const override { return "MouseButtonReleased"; }
+	};
+}
+
+#endif /* MOUSE_EVENT_H */
